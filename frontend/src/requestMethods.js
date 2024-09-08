1fc69b8e0ebd883661e637dde33345b35d6caf64
josephmwalai <josephmwalai@gmail.com>
2024-09-08 16:31:59 +0300

    updates

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+// Use environment variable for base URL
+const BASE_URL = import.meta.env.VITE_BACKEND_API_URL;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
